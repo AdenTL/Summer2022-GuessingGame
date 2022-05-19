1503f2dadfb4dbc3e548265f9265463da47dbbaa
AdenTL <15adenlangdon@gmail.com>
2022-05-19 14:40:32 -0400

    added parts 1 and 2 of guessing game

--- Summer2022-GuessingGame/Summer2022-GuessingGame/Program.cs
+++ Summer2022-GuessingGame/Summer2022-GuessingGame/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Adens and Thomas's Guessing Game!");
 int num = 9;
+
+
+Console.WriteLine("Guess a number between 1 - 10!: ");
+
+
+int userInput = Int32.Parse(Console.ReadLine());
+if (userInput == num)
+{
+    Console.WriteLine("You guess the correct number");
+}
